3bc27d5f87736c0e022ced95c10057c1a265aee7
Seth Stephanz <sethstephanz@gmail.com>
2025-01-11 19:50:10 -0500

    Add search, get, set, avg, max, min, sum methods

--- dynamic_array/main.c
+++ dynamic_array/main.c
@@ -21,41 +21,63 @@ void append(DynamicArray *arr, int el);          // append element to dynamic ar
 void insert(DynamicArray *arr, int el, int idx); // insert element at index
 void delete(DynamicArray *arr, int idx);         // delete element by index
 int search(DynamicArray *arr, int el);           // search for element. returns first index found
+int get(DynamicArray *arr, int idx);             // return value of element at index
+void set(DynamicArray *arr, int idx, int val);   // replace value of element at index with provided value
+float avg(DynamicArray *arr);                    // return average of values in array
+int max(DynamicArray *arr);                      // return maximum value in array
+int min(DynamicArray *arr);                      // return minimum value in array
+int sum(DynamicArray *arr);                      // return sum of values in array
 
 int main()
 {
     DynamicArray *arr = create_array(5);
 
     // test
-    int i = 0;
-    for (; i < 5; i++)
-    {
-        append(arr, i + 1);
-        print_array(arr);
-    }
+    // test append, insert, delete, print_array
+    // int i = 0;
+    // for (; i < 5; i++)
+    // {
+    //     append(arr, i + 1);
+    //     print_array(arr);
+    // }
 
-    insert(arr, 100, 0);
-    print_array(arr);
+    // insert(arr, 100, 0);
+    // print_array(arr);
 
-    for (; i < 10; i++)
-    {
-        append(arr, i + 1);
-        print_array(arr);
-    }
+    // for (; i < 10; i++)
+    // {
+    //     append(arr, i + 1);
+    //     print_array(arr);
+    // }
 
-    delete (arr, 0);
-    print_array(arr);
-    delete (arr, 100);
-    print_array(arr);
+    // delete (arr, 0);
+    // print_array(arr);
+    // delete (arr, 100);
+    // print_array(arr);
 
-    insert(arr, 1, 0);
+    // insert(arr, 1, 0);
 
-    printf("Index of 5: %d\n", search(arr, 5));
-    printf("Index of 1000: %d\n", search(arr, 1000));
+    // // test search, get, set, avg, max, min, sum
+    // printf("Index of 5: %d\n", search(arr, 5));       // 4
+    // printf("Index of 1000: %d\n", search(arr, 1000)); // not found
+
+    // printf("Get index 0: %d\n", get(arr, 0));
+    // printf("Get invalid index: %d\n", get(arr, 500));
+
+    // printf("Avg of non-empty arr: %f\n", avg(arr));
+    // printf("Max of non-empty arr: %d\n", max(arr));
+    // printf("Min of non-empty arr: %d\n", min(arr));
+    // printf("Sum of non-empty arr: %d\n", sum(arr));
+
+    // DynamicArray *empty_arr = create_array(5); // create blank array
+
+    // printf("Avg of empty arr: %f\n", avg(empty_arr));
+    // printf("Max of empty arr: %d\n", max(empty_arr));
+    // printf("Min of empty arr: %d\n", min(empty_arr));
+    // printf("Sum of empty arr: %d\n", sum(empty_arr));
 
     free_array(arr);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 DynamicArray *create_array(int initial_length)
@@ -170,4 +192,102 @@ int search(DynamicArray *arr, int el)
         }
     }
     return -1;
+}
+
+int get(DynamicArray *arr, int idx)
+{
+    if (idx >= 0 && idx <= arr->size)
+    {
+        return arr->data[idx];
+    }
+    else
+    {
+        printf("Get: Invalid index\n");
+        return -1;
+    }
+}
+
+void set(DynamicArray *arr, int idx, int val)
+{
+    if (idx >= 0 && idx <= arr->size)
+    {
+        arr->data[idx] = val;
+    }
+    else
+    {
+        printf("Set: Invalid index\n");
+    }
+}
+
+float avg(DynamicArray *arr)
+{
+    if (arr->size == 0)
+    {
+        printf("Avg: No elements in array\n");
+        return 0.0;
+    }
+    int sum = 0;
+    for (int i = 0; i < arr->size; i++)
+    {
+        sum += arr->data[i];
+    }
+    float avg = sum / arr->size;
+    return avg;
+}
+
+int max(DynamicArray *arr)
+{
+    // TODO: Figure out better way to handle no values.
+    if (arr->size == 0)
+    {
+        printf("Max: No elements in array\n");
+        return -1;
+    }
+
+    int max = arr->data[0];
+    for (int i = 1; i < arr->size; i++)
+    {
+        if (max < arr->data[i])
+        {
+            max = arr->data[i];
+        }
+    }
+    return max;
+}
+
+int min(DynamicArray *arr)
+{
+    // TODO: Figure out better way to handle no values.
+    if (arr->size == 0)
+    {
+        printf("Min: No elements in array\n");
+        return -1;
+    }
+
+    int min = arr->data[0];
+    for (int i = 1; i < arr->size; i++)
+    {
+        if (min > arr->data[i])
+        {
+            min = arr->data[i];
+        }
+    }
+    return min;
+}
+
+int sum(DynamicArray *arr)
+{
+    // TODO: Figure out better way to handle no values.
+    if (arr->size == 0)
+    {
+        printf("Sum: No elements in array\n");
+        return -1;
+    }
+
+    int sum = arr->data[0];
+    for (int i = 1; i < arr->size; i++)
+    {
+        sum += arr->data[i];
+    }
+    return sum;
 }
