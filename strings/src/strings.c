6cc55e902b47839729c04090e990d9ef61bfdebe
Seth Stephanz <sethstephanz@gmail.com>
2025-05-01 17:55:18 -0400

    Update string and da

--- strings/src/strings.c
+++ strings/src/strings.c
@@ -10,26 +10,40 @@
 // 'a' - 'z' = 97 - 122
 // 'A' - 'Z' = 65 - 90
 
-String *create_string(int str_length) {
+String *create_string(char *raw_str) {
+    int str_length = strlen(raw_str);
+
     if (str_length >= MAX_STR_LEN) { // too large or no room for null terminator
         fprintf(stderr, "Max string length is 10000 characters\n");
         exit(EXIT_FAILURE);
     }
-    String *str = (String *)malloc(sizeof(String));
+
+    String *str = malloc(sizeof(String));
     if (!str) {
         fprintf(stderr, "Failed to allocate memory for string\n");
         exit(EXIT_FAILURE);
     }
 
-    str->data = (char *)malloc(str_length * sizeof(char)); // this cast is unecessary in C, but is in C++
+    // Allocate space for null terminator
+    str->data = malloc((str_length + 1) * sizeof(char));
     if (!str->data) {
-        perror("Failed to allocate memory for data\n");
+        fprintf(stderr, "Failed to allocate memory for data\n");
         free(str);
         exit(EXIT_FAILURE);
     }
-    str->size = 0;
+
+    strcpy(str->data, raw_str); // copy string including null terminator
     str->length = str_length;
+    str->capacity = str_length + 1; // total allocated size
+
     return str;
+}
+
+int len(String *str) {
+    if (!str) {
+        return -1;
+    }
+    return str->length - 1;
 }
 
 void free_string(String *str) {
@@ -38,70 +52,72 @@ void free_string(String *str) {
 }
 
 void print_string(String *str) {
-    printf("Array (size: %d, length: %d): ", str->size, str->length);
-    for (int i = 0; i < str->size; i++) {
+    printf("Array (size: %d, length: %d): ", str->capacity, str->length);
+    for (int i = 0; i < str->capacity; i++) {
         printf("%d ", str->data[i]);
     }
     printf("\n");
 }
 
-int lower(char *str) {
-    if (!str) {
-        return -1;
-    }
-    size_t s = 0;
-    while (str[s] != '\0') {
-        int ascii = (int)str[s];
-        if (ascii >= 65 && ascii <= 90) {
-            str[s] = str[s] + 32;
-        }
-        s++;
-    }
-    return 0;
-}
+// int lower(char *str) {
+// if (!str) {
+// return -1;
+// }
+// size_t s = 0;
+// while (str[s] != '\0') {
+// int ascii = (int)str[s];
+// if (ascii >= 65 && ascii <= 90) {
+// str[s] = str[s] + 32;
+// }
+// s++;
+// }
+// return 0;
+// }
 
-int upper(char *str) {
-    if (!str) {
-        return -1;
-    }
-    size_t s = 0;
-    while (str[s] != '\0') {
-        int ascii = (int)str[s];
-        if (ascii >= 97 && ascii <= 122) {
-            str[s] = str[s] - 32;
-        }
-        s++;
-    }
-    return 0;
-}
+// int upper(char *str) {
+// if (!str) {
+// return -1;
+// }
+// size_t s = 0;
+// while (str[s] != '\0') {
+// int ascii = (int)str[s];
+// if (ascii >= 97 && ascii <= 122) {
+// str[s] = str[s] - 32;
+// }
+// s++;
+// }
+// return 0;
+// }
 
-int ltrim(char *str) {
-    if (!str || len(str) <= 0) {
-        return -1;
-    }
-    int l = 0;
-    int r = 0;
-    while (isspace(str[r])) {
-        r++;
-    }
-    while (str[r] != '\0') {
-        str[l++] = str[r++];
-    }
-    str[l] = '\0';
-    return 0;
-}
+// int ltrim(char *str) {
+// int *str_len = NULL;
+// if (!str || len(str, str_len) < 0) {
+// return -1;
+// }
+// int l = 0;
+// int r = 0;
+// while (isspace(str[r])) {
+// r++;
+// }
+// while (str[r] != '\0') {
+// str[l++] = str[r++];
+// }
+// str[l] = '\0';
+// return 0;
+// }
 
-int rtrim(char *str) {
-    int r = len(str);
-    if (!str || r <= 0) {
-        return -1;
-    }
-    while (isspace(str[r])) {
-        r--;
-    }
-    str[r] = '\0';
-    return 0;
-}
+// int rtrim(char *str) {
+// int *str_len = NULL;
+// if (!str || len(str, str_len) < 0) {
+// return -1;
+// }
+// int r = str_len;
+// while (isspace(str[r])) {
+// r--;
+// }
+// str[r] = '\0';
+// return 0;
+// }
 
 // void trim(char *str) {
 // ltrim(str);
--- strings/src/strings.h
+++ strings/src/strings.h
@@ -6,17 +6,17 @@
 #define MAX_STR_LEN 10000
 
 typedef struct {
-    int *data;
+    char *data;
     int capacity; // number of places in memory in array
     int length;   // number of elements in the array
 } String;
 
 // init & utility functions
-String *create_string(int initial_length); // initialize string
-void free_string(String *str);             // free memory allocated for array
-void print(String *str);                   // print contents from array
+String *create_string(char *raw_str); // initialize string
+void free_string(String *str);        // free memory allocated for array
+void print(String *str);              // print contents from array
 
-size_t len(char *str);                                           // return length of string. -1 if invalid entry
+int len(String *str);                                            // return length of string
 int lower(char *str);                                            // convert all chars in string to lower case
 int upper(char *str);                                            // convert all chars in string to upper case
 int ltrim(char *str);                                            // trim white space from left
@@ -31,5 +31,6 @@ size_t find(char *str, char *to_find);                           // find first i
 size_t replace(char *str, char *to_replace, char *replace_with); // replace first instance of character in string
 int split(char *str, char *delimeter);                           // split string on
 int join(char *str, char *token, char *delimeter);               // string will be joined using token
+int reverse(char *str);                                          // reverses string
 
 #endif // STRINGS_H
