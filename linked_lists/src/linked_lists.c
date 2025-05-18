e59ac3aa4bee153cd141ed5cf5620ee9918297aa
Seth Stephanz <sethstephanz@gmail.com>
2025-05-17 22:19:42 -0400

    Add LL init and basic methods

--- linked_lists/src/linked_lists.c
+++ linked_lists/src/linked_lists.c
@@ -2,9 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-Node *node() {
-    struct Node *p;
-    p = (struct Node *)malloc(sizeof(Node));
-    return p;
+Node *create_node(int data) {
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
 }
 
+int insert_node(Node *head, int data) {
+    if (!head) {
+        return -1;
+    }
+    while (head->next) {
+        head = head->next;
+    }
+    Node *new_node = create_node(data);
+    if (!new_node) {
+        return -1;
+    }
+    head->next = new_node;
+    new_node->next = NULL;
+    return 0;
+}
+
+void print_linked_list(Node *node) {
+    fprintf(stderr, "----------\nBEGIN LIST:\n----------\n");
+    while (node->next) {
+        fprintf(stderr, "%d\n", node->data);
+        node = node->next;
+    }
+    fprintf(stderr, "----------\nEND LIST:\n----------\n");
+}
